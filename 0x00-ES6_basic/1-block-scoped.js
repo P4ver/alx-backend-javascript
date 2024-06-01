2ce763ca44ee7e2398348a4fe99bea1f0f186655
P4ver <s.pales20@gmail.com>
2024-06-01 10:51:43 +0100

    new tsk

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
+  const task = false;
+  const task2 = true;
   
-    if (trueOrFalse) {
-      const task = true; // eslint-disable-line no-unused-vars
-      const task2 = false; // eslint-disable-line no-unused-vars
-    }
-  
-    return [task, task2];
+  if (trueOrFalse) {
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
+  
+  return [task, task2];
+}
